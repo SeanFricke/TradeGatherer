10aff0d55b70fbc03474b42ad8a875a6c074648b
Sean Fricke <Seanfricke03@gmail.com>
2024-02-23 11:07:31 -0500

    make seperate test file for clean boot tests

---
+++ src/test_firstBoot.py
@@ -0,0 +1,24 @@
+import os.path
+import pytest
+import api
+
+
+@pytest.fixture
+def test_clean_config():
+    if os.path.exists("config.txt"):
+        os.remove("config.txt")
+
+
+def test_config_make(test_clean_config):
+    test_clean_config
+    testMarket = api.MarketAPI()
+    del testMarket
+    assert os.path.exists("config.txt")
+
+
+def test_apiData_make(test_clean_config):
+    test_clean_config
+    testMarket = api.MarketAPI()
+    dataDir = testMarket.dataDir
+    del testMarket
+    assert os.path.exists(os.path.join(dataDir, "ApiData.json"))
