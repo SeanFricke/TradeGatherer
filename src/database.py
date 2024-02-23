79a112b6cbb7fd87968f39eee2543fd893dc9bf2
Sean Fricke <Seanfricke03@gmail.com>
2024-02-22 23:48:18 -0500

    add database.py and basic data pull feature

---
+++ src/database.py
@@ -0,0 +1,9 @@
+import os
+import pandas as pd
+import numpy as np
+
+
+class Database:
+    def __init__(self, dataPath, afPath):
+        self.apiPath, self.afPath = os.path.join(dataPath, "ApiData.json"), afPath  # Set file paths as instance methods
+        self.apiData = pd.read_json(self.apiPath)
