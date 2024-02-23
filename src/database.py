fa398cdc81c9199ac627dff5230b83f6440fde01
Sean Fricke <seanfricke03@gmail.com>
2024-02-23 11:24:58 -0500

    Make new main test file
    Signed-off-by: Sean Fricke <Seanfricke03@gmail.com>

--- src/database.py
+++ src/database.py
@@ -1,9 +1,12 @@
 import os
 import pandas as pd
 import numpy as np
+import utils
 
 
 class Database:
-    def __init__(self, dataPath, afPath):
-        self.apiPath, self.afPath = os.path.join(dataPath, "ApiData.json"), afPath  # Set file paths as instance methods
+    def __init__(self):
+        self.afPath, self.dataPath, temp = None  # Set instance attrbute to none
+        temp, self.dataPath, self.afPath .readConfig()  # Get data paths from config
+        self.apiPath= os.path.join(self.dataPath, "ApiData.json")  # Get local api path from data directory
         self.apiData = pd.read_json(self.apiPath)  # Make dataframe
