5cbbbe5f4ed17a890c0b97b6833e1b647340d929
Sean Fricke <seanfricke03@gmail.com>
2024-02-24 10:51:52 -0500

    create seperate JSON-to-Dataframe function in utils in prep for reusage with multiple features

--- src/Utils/database.py
+++ src/Utils/database.py
@@ -1,22 +1,14 @@
-import json
-import os
 from pathlib import Path
-
 import pandas as pd
-import numpy as np
 from src.Utils import utils
 
 
 class Make:
-    # TODO get json file to load into a dataframe of items properly
     def __init__(self):
         self.afPath = ""  # Clear Alecaframe data path
         self.apiPath = ""  # Clear Data directory path
+        self.apiData = None
+        self.orderData = None
         temp, self.dataPath, self.afPath = utils.readConfig()  # Get data paths from config
         self.apiPath = self.dataPath + "/ApiData.json"  # Get local api path from data directory
-        raw_json_dict = json.loads(Path(self.apiPath).read_text())
-        self.apiData = pd.DataFrame()
-        for key in raw_json_dict["payload"]["items"]:
-            # Grabs the json entry and puts it into a Series object
-            # Casts the Series object into a Dataframe and then concats to the apiData.
-            self.apiData = pd.concat([self.apiData, pd.DataFrame([pd.Series(key)])])
+        self.apiData = utils.getDFFromAPIJSON(Path(self.apiPath).read_text())  # Create dataframe from api file
