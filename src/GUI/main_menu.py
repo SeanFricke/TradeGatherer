5551951758216d948665936a16e1588221a530b7
Sean Fricke <seanfricke03@gmail.com>
2024-04-09 22:20:56 -0400

    In progress, test only

--- src/GUI/main_menu.py
+++ src/GUI/main_menu.py
@@ -1,18 +1,34 @@
 import sys
+
+import PyQt5.QtCore
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
+from src.Utils import api, database, utils
 
-class mainMenu:
-    app = QApplication(sys.argv)
-    screen = app.primaryScreen()
-    screen_size = screen.size()
 
-    win = QMainWindow()
+class mainMenu(QApplication):
+
+    def __init__(self, api_obj: api.API, db_obj: database.Database):
+        super().__init__(sys.argv)
+        self.__api = api_obj
+        self.__db = db_obj
+        self.screen = self.primaryScreen()
+        self.screen_size = self.screen.size()
+        self.win = QMainWindow()
+
+        self.__items_box = QGroupBox(self.win)
+        self.__items_box_layout = QVBoxLayout()
+        self.create_item_buttons()
+        self.__items_box.setLayout(self.__items_box_layout)
+        self.__items_box.setFixedWidth(self.screen_size.width())
+
+        self.scroll_area = QScrollArea(self.win)
+        self.scroll_area.setWidget(self.__items_box)
+        self.scroll_area.setFixedSize(self.screen_size)
 
     def main(self, fullscreen: bool):
-
         self.win.setGeometry(0, 0, self.screen_size.width(), self.screen_size.height())
         self.win.setWindowTitle("Trade Gatherer")
 
@@ -20,9 +36,14 @@ class mainMenu:
             self.win.showFullScreen()
         else:
             self.win.show()
-        sys.exit(self.app.exec_())
+        sys.exit(self.exec_())
 
-
-if __name__ == "__main__":
-    menu = mainMenu()
-    menu.main(False)
+    def create_item_buttons(self):
+        button_iter = 0
+        for item in self.__db.items_df.keys():
+            button = QPushButton(self.win)
+            button.resize(self.screen_size.width(), 200)
+            button.setText(item)
+            button.setCheckable(True)
+            self.__items_box_layout.addWidget(button, button_iter)
+            button_iter += 1
